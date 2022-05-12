e158a2a00c3fb58611b74146bf062cf259463ea8
jonhealy1 <jonathan.d.healy@gmail.com>
2022-05-12 05:48:22 -0700

    display folium map

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import folium
+import geopandas
+from shapely import wkt
+from matplotlib import pyplot
+from shapely.geometry.polygon import LinearRing, Polygon
+from streamlit_folium import st_folium
 
 from google.cloud import bigquery
 import os
@@ -36,10 +42,34 @@ data_load_state = st.text('Loading data...')
 data = load_data()
 data_load_state.text('Loading data...done!')
 
-st.subheader('Map')
-# st.map(data)
-st.map(data=None, zoom=None, use_container_width=True)
+# st.subheader('Map')
+# # st.map(data)
+# st.map(data=None, zoom=None, use_container_width=True)
 
-st.subheader('Raw data')
-st.write(data)
+# st.subheader('Raw data')
+# st.write(data)
 
+df = pd.DataFrame(
+    {
+        'id': [row["id"] for row in data],
+        'geometry': [row["geom"] for row in data]
+    }
+)
+
+df['geometry'] = geopandas.GeoSeries.from_wkt(df['geometry'])
+
+gdf = geopandas.GeoDataFrame(df, geometry='geometry')
+
+print(gdf.head())
+
+# gdf.plot()
+
+# plt.show()
+
+gdf.loc[5, "geometry"]
+
+m = folium.Map(location=[40.70, -73.94], zoom_start=10, tiles='CartoDB positron')
+
+st_data = st_folium(m, width = 725)
+# st.set_option('deprecation.showPyplotGlobalUse', False)
+# st.pyplot()
