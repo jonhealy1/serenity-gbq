aaa0773e8bb73684cf1dba16a4c61a02ff2ed46a
jonhealy1 <jonathan.d.healy@gmail.com>
2022-05-12 04:16:35 -0700

    create geodataframe

--- serenity-gbq.py
+++ serenity-gbq.py
@@ -1,6 +1,14 @@
 import json
 import os
 from shapely.geometry import shape
+import pandas
+import geopandas
+from shapely import wkt
+from google.cloud.bigquery.schema import SchemaField
+import google.cloud.bigquery.job
+from google.cloud import bigquery
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="creds/serenity-gbq-4f1a4edba243.json"
 
 def geojson_to_wkt(geojson: dict) -> str:
     """Convert geojson to wkt."""
@@ -13,21 +21,28 @@ def load_data(filename: str) -> dict:
     with open(filename) as file:
         return json.load(file)
 
-def load_items() -> list:
-    """Load stac items, convert"""
+def load_geodataframe(bigquery_client, dataset_id):
     feature_collection = load_data("data/stac-data/sentinel-s2-l2a-cogs_0_100.json")
-    data_list = []
 
-    for feature in feature_collection["features"]:
-        data_list.append(
-            {
-                "id": feature["id"],
-                "wkt": geojson_to_wkt(feature["geometry"])
-            }
-        )
-        
-    return data_list
+    df = geopandas.GeoDataFrame(
+        pandas.DataFrame(
+            dict(
+                name=[data["id"] for data in feature_collection["features"]],
+                geo1=[wkt.loads(geojson_to_wkt(data["geometry"])) for data in feature_collection["features"]],
+            )
+        ),
+        geometry="geo1",
+    )
+    print(df)
 
-data_list = load_items()
+    # table_id = f"{dataset_id}.test_stac"
+    # bigquery_client.load_table_from_dataframe(df, table_id).result()
 
-print(data_list)
+    # table = bigquery_client.get_table(table_id)
+    # assert table.schema == [
+    #     SchemaField("name", "STRING", "NULLABLE"),
+    #     SchemaField("geo1", "GEOGRAPHY", "NULLABLE"),
+    # ]
+ 
+client = bigquery.Client()
+load_geodataframe(client, "waterways")
